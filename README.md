1985339978d4d4f656d8f397fa513ae81dff6c7f
Fatih Furkan Hatipoglu <fatihfurkn@gmail.com>
2021-05-01 21:34:39 +0300

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+# I created this repo for projects that i build in my blockchain learning process.
---
+++ firstSmartContract/FirstContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract FirstContract {
+    function getInteger() public pure returns(uint) {
+        return 123;
+    }
+}
