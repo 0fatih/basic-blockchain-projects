d01711121614bd26bda4fa5a5f14ea2c2eec4a70
0fatih <48567409+0fatih@users.noreply.github.com>
2021-05-03 18:29:57 +0300

    Create RemotePurchase.sol

---
+++ solidity-docs/remote-purchase/RemotePurchase.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract RemotePurchase {
+    
+    uint public value;
+    address payable public seller;
+    address payable public buyer;
+    
+    enum State { Created, Locked, Release, Inactive }
+    State public state;
+    
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
+    
+    error OnlyBuyer(); // Only the buyer call this function
+    error OnlySeller(); // Only the seller call this function
+    error InvalidState(); // The function cannot be called at the current state
+    error ValueNotEven(); // The provided value has to be even
+    
+    modifier onlyBuyer() {
+        if(msg.sender != buyer)
+            revert OnlyBuyer();
+        _;
+    }
+    
+    modifier onlySeller() {
+        if(msg.sender != seller)
+            revert OnlySeller();
+        _;
+    }
+    
+    modifier inState(State _state) {
+        if(state != _state)
+            revert InvalidState();
+        _;
+    }
+    
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+    
+    constructor() payable {
+        seller = payable(msg.sender);
+        value = msg.value / 2;
+        if((2*value) != msg.value)
+            revert ValueNotEven();
+    }
+    
+    function abort() public onlySeller inState(State.Created) {
+        
+        emit Aborted();
+        state = State.Inactive;
+        seller.transfer(address(this).balance);
+    }
+    
+    function confirmPurchase() public inState(State.Created) condition(msg.value == (2*value)) payable {
+        emit PurchaseConfirmed();
+        buyer = payable(msg.sender);
+        state = State.Locked;
+    }
+    
+    function confirmReceived() public onlyBuyer inState(State.Locked) {
+        emit ItemReceived();
+        state = State.Release;
+        buyer.transfer(value);
+    }
+    
+    function refundSeller() public onlySeller inState(State.Release) {
+        emit SellerRefunded();
+        state = State.Inactive;
+        seller.transfer(3*value);
+    }
+    
+}
