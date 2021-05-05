51a7a166ba0543741b8506d2dbeadd6051b7ae69
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-06 00:34:53 +0300

    Create CreatingContractsViaNEW.sol

---
+++ solidity-docs/expressions-and-control-structures/CreatingContractsViaNEW.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+contract D {
+    uint public x;
+    
+    constructor(uint a) payable {
+        x = a;
+    }
+}
+
+contract C {
+    D d = new D(4); // 4 will be executed as part of constructor of D
+    
+    function createD(uint arg) public {
+        D newD = new D(arg);
+        newD.x();
+    }
+    
+    function createAndEndowD(uint arg, uint amount) public payable {
+        // Send ether along with the creation
+        D newD = new D{value: amount}(arg);
+        newD.x();
+    }
+}
