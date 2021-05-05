3184c95c6ccbd26585d1ef7287e1ea0fc27d44d2
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-05 22:27:12 +0300

    Create InternalFunctionCalls.sol

---
+++ solidity-docs/expressions-and-control-structures/InternalFunctionCalls.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+contract InternalFunctionCalls {
+    
+    function g(uint a) public pure returns (uint ret) { return a + f(); } // We can access this function out of contract
+    function f() internal pure returns (uint ret) { return g(7) + f(); } // But we can't access that one
+    
+}
