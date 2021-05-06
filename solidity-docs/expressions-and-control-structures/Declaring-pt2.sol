ffafabefdd6ad97129ff36d1ddece7c38bad6268
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-06 20:27:29 +0300

    Create Declaring-pt2.sol

---
+++ solidity-docs/expressions-and-control-structures/Declaring-pt2.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+contract C {
+    
+    // This will report a warning
+    function declare() pure public returns(uint) {
+        
+        uint x = 1;
+        
+        {
+            x = 2; // this will assign to the outer variable
+            uint x;
+        }
+        
+        return x; // x has value 2
+        
+    }
+    
+}
