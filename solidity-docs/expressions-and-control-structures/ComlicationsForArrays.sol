bf9048a7a8f0dbe49d014e81505abc9854efff31
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-06 04:11:34 +0300

    Create ComlicationsForArrays.sol

---
+++ solidity-docs/expressions-and-control-structures/ComlicationsForArrays.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+contract C {
+    uint[20] x;
+    
+    function f() public {
+        g(x);
+        h(x);
+    }
+    
+    /* 
+    
+        In the example below the call to g(x) has no effect on x because it creates an 
+        independent copy of the storage value in memory. However, h(x) successfully 
+        modifies x because only a reference and not a copy is passed.
+
+    */
+    
+    function g(uint[20] memory y) internal pure {
+        y[2] = 3;
+    }
+    
+    function h(uint[20] storage y) internal {
+        y[3] = 4;
+    }
+}
