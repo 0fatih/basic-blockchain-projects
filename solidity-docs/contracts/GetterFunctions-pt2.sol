5143f0201d81efd83dfc299847437f563c693c33
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-07 05:01:46 +0300

    Create GetterFunctions-pt2.sol

---
+++ solidity-docs/contracts/GetterFunctions-pt2.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+
+    /*
+    
+                            Getter
+        
+        The getter functions have external visibility. If the symbol is accessed internally (i.e.
+        without `this`), it evaluates to a state variable. If it is accessed externally (i.e.. with `this`)
+        it evaluates to a function.
+    
+    */
+    
+
+contract C {
+    uint public data;
+    
+    function x() public returns (uint) {
+        data = 3; // internal access 
+        return this.data(); // external access
+    }
+}
