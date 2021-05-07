39186893c6cb45d8b82109e6549b7477dbba11cd
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-07 04:57:40 +0300

    Create Getter-pt1.sol

---
+++ solidity-docs/contracts/Getter-pt1.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+
+    /*
+    
+                            Getter
+        
+        The compiler automatically creates getter functions for all public state variables.
+        For the contract given below, the compiler will generate a function called `data`
+        that does not take any arguments and returns a `uint`, the value of the state variable `data`.
+        State variables can be initialized when they are declared.
+    
+    */
+    
+
+contract C {
+    
+    uint public data = 42;
+}
+
+contract Caller {
+    C c = new C();
+    function f() public view returns (uint) {
+        return c.data();
+    }
+}
