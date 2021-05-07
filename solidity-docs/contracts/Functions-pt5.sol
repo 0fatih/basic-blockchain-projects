179941721811a85f15519f961e675131531b84fa
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-08 00:31:20 +0300

    Update Functions-pt5.sol

--- solidity-docs/contracts/Functions-pt5.sol
+++ solidity-docs/contracts/Functions-pt5.sol
@@ -4,25 +4,35 @@ pragma solidity ^0.8.4;
 
 /*
 
-                            View Functions
+                            Pure Functions
                             
-    Functions can be declared view in which case they promise not to modify the state.
-
-    The following statements are considered modifying the state:
-
-        1) Writing to state variables.
-        2) Emitting events.
-        3) Creating other contracts.
-        4) Using `selfdestruct`.
-        5) Sending Ether via calls.
-        6) Calling any function not marked `view` or `pure`.
-        7) Using low-level calls.
-        8) Using inline assembly that contains certain opcodes.
+    Functions can be declared `pure` in which case they promise not to read from or modify the state.
+    
+    In addition to the list of state modifying statements explained above, the following are considered
+    reading from the state:
+        
+        1) Reading from state variables.
+        2) Accessing `address(this).balance` or `<address>.balance`.
+        3) Accessing any of the memebers of `block`, `tx`, `msg`(with the exception of msg.sig and msg.data).
+        4) Calling any function not marked `pure`.
+        5) Using inline assembly that contains certain opcodes.
 */
 
 contract C {
      
-     function f(uint a, uint b) public view returns (uint) {
-         return a * (b + 42) + block.timestamp;
+     function f(uint a, uint b) public pure returns (uint) {
+         return a * (b + 42);
      }
 }
+
+/*
+    
+    Pure functions are able to use the revert() and require() functions to revert potentiak state changes when an error occurs.
+    
+    Reverting a state change is not considered a 'state modification', as only changes to the state made previously
+    in code that did not have the `view` or `pure` restriction are reverted and that code has the option to catch the 
+    revert and not pass it on.
+    
+    This behaviour is also in line with the `STATICCAL` opcode.
+
+*/
