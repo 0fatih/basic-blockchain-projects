af9937e7c43889d8549fa73f30bda843611eb6fb
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-08 23:19:23 +0300

    Create FunctionOverloading-pt2.sol

---
+++ solidity-docs/contracts/FunctionOverloading-pt2.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+/*
+
+                                Function Overloading
+                                
+    Overloaded functions are also present in the external interface. It is an error if 
+    two externally visible functions differ by their Solidity types but not by their external types.
+
+
+
+*/
+
+
+// This will not compile
+contract A {
+    
+    function f(B _in) public pure returns (B out) {
+        out = _in;
+    }
+    
+    function f(address _in) public pure returns (address out) {
+        out = _in;
+    }
+    
+}
+
+contract B {
+    
+}
