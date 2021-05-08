67123cd1cb74bf9cd4530102b607883591cf1175
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-08 23:04:58 +0300

    Update Faucet.sol

--- mastering-ethereum-book/Faucet.sol
+++ mastering-ethereum-book/Faucet.sol
@@ -14,7 +14,7 @@ contract Faucet {
                 msg.sender.transfer(withdraw_amount);
             
             But it gives me an address payable error. I am in learning process and i found this solution for now.
-            For now i don't know will it work. If i won't forget, i will take a look later.
+            It works.
         
         */
         payable(msg.sender).transfer(withdraw_amount);
