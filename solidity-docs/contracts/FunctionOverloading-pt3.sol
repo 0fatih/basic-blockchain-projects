9d382c5dafb1cb96fc7bbfe397e3c6b943524a47
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-08 23:23:16 +0300

    Create FunctionOverloading-pt3.sol

---
+++ solidity-docs/contracts/FunctionOverloading-pt3.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+
+contract A {
+    
+    function f(uint8 _in) public pure returns (uint8 out) {
+        out = _in;
+    }
+    
+    function f(uint256 _in) public pure returns (uint256 out) {
+        out = _in;
+    }
+    
+}
+
+/*
+
+    Calling f(50) would create a type error since 50 can be 
+    implicitly converted both to uint8 and uint256 types. On 
+    another hand f(256) would resolve to f(uint256) overload 
+    as 256 cannot be implicitly converted to uint8.
+
+*/
