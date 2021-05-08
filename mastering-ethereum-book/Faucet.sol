3eec90d80381f973d17c76867fcb5f666254dd50
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-08 21:22:54 +0300

    Create Faucet.sol

---
+++ mastering-ethereum-book/Faucet.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+contract Faucet {
+    
+    receive() external payable {}
+    
+    function withdraw(uint withdraw_amount) public {
+        require(withdraw_amount <= 0.1 ether);
+        
+        /*
+            The original code is:
+                msg.sender.transfer(withdraw_amount);
+            
+            But it gives me an address payable error. I am in learning process and i found this solution for now.
+            For now i don't know will it work. If i won't forget, i will take a look later.
+        
+        */
+        payable(msg.sender).transfer(withdraw_amount);
+        
+    }
+    
+}
