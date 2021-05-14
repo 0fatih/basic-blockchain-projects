423d14d376409e7c3a78f979cdd33fc701fa04fc
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-14 09:39:30 +0300

    Create ShadowingInheritedStateVariables.sol

---
+++ solidity-by-example/ShadowingInheritedStateVariables.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.4;
+
+
+/*
+
+                        Shadowing Inherited State Variables
+    
+    Unlike functions, state variables cannot be overridden by re-declaring it in the child contract.
+    
+    Let's learn how to correctly override inherited state variables.
+
+*/
+
+contract A {
+    string public name = "Contract A";
+    
+    function getName() public view returns(string memory) {
+        return name;
+    }
+}
+
+contract B is A {
+    constructor() {
+        name = "Contract B";
+    }
+}
