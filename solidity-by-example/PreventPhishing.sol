6a8102bed8b7dd84c8e4efde318788fdfd95c9be
Fatih <48567409+0fatih@users.noreply.github.com>
2021-05-16 04:55:34 +0300

    Create PreventPhishing.sol

---
+++ solidity-by-example/PreventPhishing.sol
@@ -0,0 +1,9 @@
+/* Use msg.sender instead of tx.origin
+
+  function transfer(address payable _to, uint _amount) public {
+        require(msg.sender == owner, "Not owner");
+
+        (bool sent, ) = _to.call.value(_amount)("");
+        require(sent, "Failed to send Ether");
+    }
+*/
